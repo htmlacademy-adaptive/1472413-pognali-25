b70cddb88ce3ec094146ee2720505ba0cd9117ed
Kulinich Ekaterina <kulinich.ekaterina.andreevna@gmail.com>
2022-05-11 18:16:37 +0300

    adding pictures, js

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,17 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+let header = document.querySelector('.header');
+let headerNav = document.querySelector('.header__nav');
+let burger = document.querySelector('.burger');
 
-navMain.classList.remove('main-nav--nojs');
+header.classList.remove('header--no-js');
+headerNav.classList.remove('nav--open');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+burger.addEventListener('click', function () {
+  console.log("1");
+  if (!headerNav.classList.contains('nav--open')) {
+    headerNav.classList.add('nav--open');
+    burger.classList.add('burger--open');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    headerNav.classList.remove('nav--open');
+    burger.classList.remove('burger--open');
   }
 });
